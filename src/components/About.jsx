ddfe39434f495ea14b1fa7ffed39732646744968
kalwhyte <superkalel55@gmail.com>
2023-09-30 00:13:41 +0100

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        I'm Ejiofor Emmanuel Arinze, a passionate and driven software engineer currently pursuing my career in backend development. I have a strong foundation in software engineering, and I am continually seeking opportunities to expand my knowledge and skills. I am a fast learner and a team player who is proficient in an array of scripting languages and frameworks. I am also a self-motivated, hardworking, and goal-oriented individual who is keen on solving problems and delivering results.
+        
+        I am a finalist in the ALX Software Engineering program, where I have gained valuable insights into modern software development practices. Additionally, I had the privilege of interning as a backend developer at HNG, where I further honed my skills and contributed to real-world projects. 
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
