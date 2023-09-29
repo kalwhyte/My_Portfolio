d158d174e38a9f60b3b82dba7fa7544127931bd7
kalwhyte <superkalel55@gmail.com>
2023-09-30 00:54:42 +0100

    portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,6 +26,10 @@ const Contact = () => {
     setLoading(true);
 
     // sign up on emailjs.com (select the gmail service and connect your account).
+    const serviceID = 'service_1i3fwvc';
+    const templateID = 'template_0t7r7kd';
+    const yourpublickey = 'DRDlZlh8rm4q4brXj';
+
     //click on create a new template then click on save.
     emailjs
       .send(
@@ -33,9 +37,9 @@ const Contact = () => {
         'templateID', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Ejiofor Emmanuel Whyte', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'superkalel55@gmail.com', //put your email here.
           message: form.message,
         },
         'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
